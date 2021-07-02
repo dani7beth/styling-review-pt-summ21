9133d19cc56dcf3d4c8daf8604b4469fac0f6d89
dani7beth <danielledona28@gmail.com>
2021-07-02 17:41:16 -0600

    things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="fullname">John Doe</h1>
+      {/* <div className="container">
+        <div className="box1"></div>
+        <div className="box2"></div>
+        <div className="box3"></div>
+      </div> */}
+      <Button>About Me</Button>
+      <a href="http://www.linkedin.com">
+        <Icon />
+      </a>
     </div>
   );
 }
-
 export default App;
+export const Icon = styled(LinkedInIcon)`
+  font-size: 7em !important;
+  position: absolute;
+  bottom: 100px;
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+`;
